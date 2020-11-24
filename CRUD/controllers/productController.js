7496409e03ff02ddfd8209b672aa88f529ad66ff
Luciano Serenellini <serenelliniluciano@gmail.com>
2020-11-24 01:10:07 -0300

    Terminado pero no logro que se guarden las ediciones, después está todo perfecto

--- CRUD/controllers/productController.js
+++ CRUD/controllers/productController.js
@@ -1,17 +1,67 @@
 const fs = require('fs');
-var productos = JSON.parse(fs.readFileSync(__dirname + "/../database/productos.json"));
+const productos = JSON.parse(fs.readFileSync(__dirname + "/../database/productos.json"));
 
-const productController = {
+const productController = 
+{
 crear : function (req, res, next){
     res.render ('crear')
 },
 store : function (req, res, next){
     productos.push(req.body);
-    productosJSON = JSON.stringify(productos);
+    let productosJSON = JSON.stringify(productos);
     fs.writeFileSync(__dirname + "/../database/productos.json", productosJSON);
-    res.send ("Producto creado");
+    res.redirect ("/productos/lista");
 
 },
+editar : function (req, res, next){
+    let idProducto = req.params.id;
+    let productoEncontrado;
+    for (var i = 0; i < productos.length; i++) {
+        if (productos[i].id == idProducto){
+            productoEncontrado = productos[i];
+            break;
+        }
+    }
+
+    if (productoEncontrado) {
+        res.render ("editar", {productoEncontrado}) 
+    
+    } else {
+        res.send ("El producto no existe");
+    }
+},
+
+update : function (req, res, next){
+    let idProducto = req.params.id;
+    
+     let editProducto = productos.map (function (producto) {
+         if (productos.id == idProducto) {
+             return req.body;
+         }
+         return producto;
+     });
+     editProductoJSON = JSON.stringify(editProducto);
+     fs.writeFileSync(__dirname + "/../database/productos.json", editProductoJSON);
+     res.redirect ("/productos/lista");
+
+
+    },
+    eliminar : function (req,res,next) {
+        let idProducto = req.params.id;
+        let productosEliminados = productos.filter(function (producto) {;
+        return producto.id != idProducto;
+
+    });
+    productosEliminadosJSON = JSON.stringify(productosEliminados)
+    fs.writeFileSync(__dirname + "/../database/productos.json", productosEliminadosJSON);
+    res.redirect ("/productos/lista");
+
+   },
+
+   lista : function (req,res,next) {
+       res.render ("lista",{productos});
+   }
+
 
 }
 
--- CRUD/routes/productos.js
+++ CRUD/routes/productos.js
@@ -7,4 +7,15 @@ router.get('/crear', productController.crear);
 router.post('/crear', productController.store);
 
 
+// formulario de edición, en este caso de productos
+router.get('/editar/:id', productController.editar);
+router.post('/editar/:id', productController.update);
+
+// eliminar
+router.get('/eliminar/:id', productController.eliminar);
+
+// ver lista de productos
+router.get('/lista', productController.lista);
+
+
 module.exports = router;
